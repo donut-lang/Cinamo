2c1ddc88b99dfeb442e3990c85539449cd4bda99
psi <ledyba@users.sourceforge.jp>
2013-03-01 18:34:04 +0900

    ライセンスは別所で書くことにした

--- runtest.sh
+++ runtest.sh
@@ -3,9 +3,6 @@
 # Tarte
 #
 # Copyright 2012-2013, PSI
-# Dual licensed under the BSD or GPL Version 3 (or any later) licenses.
-# !! GPL is STRONGLY recommended !!
-# see: http://donut-lang.org/license/
 
 DIRNAME=$(cd $(dirname $0);pwd)
 TEST_FILE="tarte_test"
--- src/FileSystem.cpp
+++ src/FileSystem.cpp
@@ -3,9 +3,6 @@
  * Tarte
  *
  * Copyright 2012-2013, PSI
- * Dual licensed under the BSD or GPL Version 3 (or any later) licenses.
- * !! GPL is STRONGLY recommended !!
- * see: http://donut-lang.org/license/
  */
 
 #include <tarte/FileSystem.h>
