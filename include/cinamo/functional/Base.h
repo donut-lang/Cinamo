b822d178ec43d05325b3d8e80fb1980d957d29fe
psi <ledyba@users.sourceforge.jp>
2013-08-15 17:53:34 +0900

    Unit Type

---
+++ include/cinamo/functional/Base.h
@@ -0,0 +1,18 @@
+/* coding: utf-8 */
+/**
+ * Cinamo
+ *
+ * Copyright 2012-2013, PSI
+ */
+
+#pragma once
+
+namespace cinamo {
+
+class Unit final{
+	operator void(){
+	}
+} Unit;
+typedef decltype(Unit) UniT;
+
+}
