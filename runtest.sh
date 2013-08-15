236081ab1a992b67e8b9e9591328570b882cfcfe
psi <ledyba@users.sourceforge.jp>
2013-08-15 18:19:33 +0900

    add gdb mode

--- runtest.sh
+++ runtest.sh
@@ -7,6 +7,14 @@
 DIRNAME=$(cd $(dirname $0);pwd)
 TEST_FILE="cinamoTest"
 
+IS_DEBUG=0
+
+for arg in $*; do
+	if [ $arg == "debug" ]; then
+		IS_DEBUG=1
+	fi
+done
+
 function run() {
 	PROG=$1/$TEST_FILE
 	MODE=$2
@@ -16,9 +24,15 @@ function run() {
 		echo "------------------------------------------------------------"
 		echo "Running $MODE test"
 		echo "------------------------------------------------------------"
-		ECMD="$PROG $*"
-		echo "%" $ECMD
-		$ECMD
+		if [ 0 -eq $IS_DEBUG ]; then
+			ECMD="$PROG $*"
+			echo "%" $ECMD
+			$ECMD
+		else
+			ECMD="gdb --args $PROG $*"
+			echo "%" $ECMD
+			$ECMD
+		fi
 	else
 		echo "not found: $PROG"
 		return 1
