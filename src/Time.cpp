f8de1d27003da1ec4208b14c3bac8dfc7dfdd345
psi <ledyba@users.sourceforge.jp>
2013-08-18 14:12:58 +0900

    add time library

---
+++ src/Time.cpp
@@ -0,0 +1,42 @@
+/* coding: utf-8 */
+/**
+ * Cinamo
+ *
+ * Copyright 2012-2013, PSI
+ */
+
+#include <cinamo/Time.h>
+#if CINAMO_WINDOWS
+#include <sys/timeb.h>
+#endif
+#if CINAMO_LINUX
+#include <sys/time.h>
+#endif
+
+namespace cinamo {
+
+#if HAVE_ULONGLONG
+unsigned long long getTimeInMills()
+{
+	typedef unsigned long long Ret;
+#else
+unsigned long getTimeInMills();
+{
+	typedef unsigned long long Ret;
+#endif
+
+#if CINAMO_LINUX
+	struct sys_time_t t;
+	gettimeofday(&t, NULL);
+	return Ret(t.tv_sec) * 1000 + Ret(t.tv_usec) / 1000;
+#endif
+
+#if CINAMO_WINDOWS
+	struct _timeb t;
+	_ftime(&t);
+	return Ret(t.time) * 1000 + Ret(t.millitm);
+#endif
+}
+
+
+}
